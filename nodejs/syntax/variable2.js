5dc65561e0e7fe1a4fff9532af92e433166b7a39
eunseo2 <20180972@sungshin.ac.kr>
2021-02-09 00:30:26 +0900

    변수의 장점

---
+++ nodejs/syntax/variable2.js
@@ -0,0 +1,7 @@
+
+var name='egoing';
+
+var letter = 'Dear' +name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate' +name+' velit esse cillum  Dear egoing dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est ' +name+' laborum.'
+
+
+console.log(letter);
