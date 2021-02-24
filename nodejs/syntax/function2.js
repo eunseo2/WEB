dbd9d3a4568fa8e8b8071b54a9dd4ae7e89e7b20
eunseo2 <20180972@sungshin.ac.kr>
2021-02-24 20:50:46 +0900

    함수의 입력

---
+++ nodejs/syntax/function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6));//2 round 반올림해줌 round(입력값)
+console.log(Math.round(1.4));//1
+
+
+function sum(first,second){ //parameter 매개변수
+  console.log(first+second)
+}
+sum(2,4); //argument
