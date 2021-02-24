219f2cadced31849f7e1a956a0ce6ad19445ce99
eunseo2 <20180972@sungshin.ac.kr>
2021-02-24 20:55:23 +0900

    함수의 입력 및 출력

--- nodejs/syntax/function2.js
+++ nodejs/syntax/function2.js
@@ -3,6 +3,10 @@ console.log(Math.round(1.4));//1
 
 
 function sum(first,second){ //parameter 매개변수
-  console.log(first+second)
+  console.log('a');
+  return first+second // return을 만나면 함수는 즉시 종료
+  console.log('b'); // 실행 안됨
 }
-sum(2,4); //argument
+
+
+console.log(sum(2,4)); //argument
