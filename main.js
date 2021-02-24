88535531469a32ddcd51d55b6c4dce7f79be687f
eunseo2 <20180972@sungshin.ac.kr>
2021-02-25 00:11:59 +0900

    함수를 이용해서 코드 정돈

--- main.js
+++ main.js
@@ -2,10 +2,38 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title,list,body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;  //URL 분석 id : css
-
     var pathname = url.parse(_url, true).pathname;
     var title = queryData.id; // .id니까 css만 나옴
 
@@ -15,14 +43,8 @@ var app = http.createServer(function(request,response){
                 // console.log(filelist);  data 디렉토리에 있는 목록들
                 var title = 'Welcome';
                 var description='Hello,Node.js';
+                var list= templateList(filelist);
 
-                var list = '<ul>';
-                var i = 0;
-                while(i < filelist.length){
-                  list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                  i = i + 1;
-                }
-                list = list+'</ul>';
                 /*
                 var list=`  <ul>
                     <li><a href="/?id=HTML">HTML</a></li>
@@ -30,21 +52,7 @@ var app = http.createServer(function(request,response){
                     <li><a href="/?id=JavaScript">JavaScript</a></li>
                   </ul>`;
                 */
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                  <title>WEB1 - ${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  ${list}
-                  <h2>${title}</h2>
-                  <p>${description}</p>
-                </body>
-                </html>
-                `;
+                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template);
             })
@@ -53,31 +61,18 @@ var app = http.createServer(function(request,response){
       } else{ //querydata id 가 있다면
 
         fs.readdir('./data', function(error, filelist){
-          var list = '<ul>';
+          /*var list = '<ul>';
           var i = 0;
           while(i < filelist.length){
             list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
             i = i + 1;
           }
           list = list+'</ul>';
-
-
+          */
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+
+          var list=templateList(filelist);
+          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
