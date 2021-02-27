e4d421f24a38568c83104cae9b5530507743472d
eunseo2 <20180972@sungshin.ac.kr>
2021-02-28 00:28:26 +0900

    수정하기

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ var app = http.createServer(function(request,response){
           var title = 'WEB- create';
           var list= templateList(filelist);
           var template = templateHTML(title,list,`
-            <form action="http://localhost:3000/create_process"  method ="post">
+            <form action="/create_process"  method ="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -110,7 +110,7 @@ var app = http.createServer(function(request,response){
       request.on('data', function(data){
           body = body + data;
       });
-      request.on('end', function(){
+      request.on('end', function(){ //data 다 받으면
         var post = qs.parse(body);
         var title = post.title;
         var description = post.description;
@@ -126,7 +126,50 @@ var app = http.createServer(function(request,response){
 
       });
 
-}
+    }else if(pathname==='/update'){
+      fs.readdir('./data', function(error, filelist){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var list=templateList(filelist);
+        var template = templateHTML(title,list,
+          `
+          <form action="/update_process"  method ="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description" ${description}></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+          </form>
+          `,
+        `<a href="/create"> create</a> <a href="/update?id=${title}"> update</a>`
+
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+     });
+   } else if(pathname ==='/update_process'){
+     var body = '';
+     request.on('data', function(data){
+         body = body + data;
+     });
+     request.on('end', function(){ //data 다 받으면
+       var post = qs.parse(body);
+       var id = post.id;
+       var title = post.title;
+       var description = post.description;
+       fs.rename(`data/${id}`, `data/${title}`, function(error){
+       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+         response.writeHead(302, {Location: `/?id=${title}`});
+         response.end();
+       })
+     });
+ });
+
+
+   }
      else {
       response.writeHead(404);
       response.end('Not found');
